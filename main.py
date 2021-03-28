d4fdd6757505a72b7c6de368fe610e5f400615d9
ingname <81479367+ingname@users.noreply.github.com>
2021-03-28 19:51:52 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,17 +1,24 @@
 from tkinter import *
 
+def clicked():
+    lbl.configure(text="Я же просил")
 
 window = Tk()
 window.title("Hexagon")
 window.geometry('900x500')
+window.iconbitmap('F:\game\Учеба\Практика\Git\Hexagon\ico\ico.ico')
 
-lbl = Label(window, text="menu:", font=("Ariak Bold", 50))
+lbl = Label(window, text="Hexagon", font=("Ariak Bold", 50))
 # Font шрифт, число размер.
 lbl.grid(column=0, row=0)
 # Местоположение
-btn = Button(window, text="Не нажимать!", fg="red")
+btn1 = Button(window, text="Начать играть", fg="black", command=clicked)
+btn2 = Button(window, text="Настройки", fg="Black")
+btn3 = Button(window, text="Выход", fg="Black", command=window.destroy)
 # Кнопка
-btn.grid(column=0, row=1)
-
+btn1.grid(column=0, row=1)
+btn2.grid(column=0, row=2)
+btn3.grid(column=0, row=3)
 
 window.mainloop()
+
