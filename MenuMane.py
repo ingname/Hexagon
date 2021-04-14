791e63f2acb464a2a3534182b0c5c679e1e7465c
ingname <81479367+ingname@users.noreply.github.com>
2021-04-14 17:40:21 +0300

    Add files via upload

--- MenuMane.py
+++ MenuMane.py
@@ -1,5 +1,4 @@
 from tkinter import *
-import pygame
 import tkinter.messagebox
 from PIL import ImageTk,Image
 import sys
@@ -14,10 +13,17 @@ photo = ImageTk.PhotoImage(image)
 image = canvas.create_image(0, 0, anchor='nw', image=photo)
 canvas.place(x=450, y=250, anchor=CENTER)
 
+
 class GameGUI(object):
 
     def __init__(self, window,):
         self.window = window
+
+        self.lbl = Label(window, text="Hexagon", font=("Ariak Bold", 50), fg="White", bg='Black')
+        self.lbl.place(x=450, y=150, anchor=CENTER)
+
+        self.txt1 = Label(window, text="ИГРА НАЧАЛАСЬ!", font=("Ariak Bold", 50), fg="White", bg='Black')
+        self.txt2 = Label(window, text="Нажмите ENTER для продолжения", font=("Ariak Bold", 20), fg="White", bg='Black')
 
         self.GameButton = Button(window, text="Начать играть", fg="White", bg='Black', font=("Ariak Bold", 15), width=12, height=1, command=self.NewGame)
 
@@ -31,23 +37,22 @@ class GameGUI(object):
 
         self.ReturnMenu = Button(window, text="Вернуться", fg="White", bg='Black', font=("Ariak Bold", 15), width=8, height=1, command=self.MainMenu)
 
-
-
-
-
         self.MainMenu()
 
     def MainMenu(self):
         self.GameButton.place(x=450, y=250, anchor=CENTER)
         self.SettingButton.place(x=450, y=295, anchor=CENTER)
         self.ExitButton.place(x=450, y=340, anchor=CENTER)
-        self.SettingSoundButton.place(x=999, y=999, anchor=CENTER)
-        self.SettingLanguageButton.place(x=999, y=999, anchor=CENTER)
-        self.ReturnMenu.place(x=999, y=999, anchor=CENTER)
+        self.SettingSoundButton.place(x=9999, y=9999, anchor=CENTER)
+        self.SettingLanguageButton.place(x=9999, y=9999, anchor=CENTER)
+        self.ReturnMenu.place(x=9999, y=9999, anchor=CENTER)
 
 
     def NewGame(self):
         self.RemoveAll()
+        self.lbl.place(x=9999, y=9999, anchor=CENTER)
+        self.txt1.place(x=450, y=250, anchor=CENTER)
+        self.txt2.place(x=450, y=300, anchor=CENTER)
         #Функция начала игры
 
     def Setting(self):
@@ -57,15 +62,19 @@ class GameGUI(object):
         self.ReturnMenu.place(x=80, y=450, anchor=CENTER)
 
     def Sound(self):
+        self.RemoveAll()
         self.ReturnMenu.place(x=80, y=450, anchor=CENTER)
 
     def Language(self):
+        self.RemoveAll()
         self.ReturnMenu.place(x=80, y=450, anchor=CENTER)
 
     def RemoveAll(self):
-        self.GameButton.place(x=999, y=999, anchor=CENTER)
-        self.SettingButton.place(x=999, y=999, anchor=CENTER)
-        self.ExitButton.place(x=999, y=999, anchor=CENTER)
+        self.GameButton.place(x=9999, y=9999, anchor=CENTER)
+        self.SettingButton.place(x=9999, y=9999, anchor=CENTER)
+        self.ExitButton.place(x=9999, y=9999, anchor=CENTER)
+        self.SettingSoundButton.place(x=9999, y=9999, anchor=CENTER)
+        self.SettingLanguageButton.place(x=9999, y=9999, anchor=CENTER)
 
     def Exit(self):
         self.window.quit
